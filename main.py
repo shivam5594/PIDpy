b7fba7aee5045c64f4c4d6fb88ddc7fd576f8f30
Shivam Chauhan <iamchauhan55@gmail.com>
2021-05-23 19:04:59 -0700

    Plotting new values to all graphs at every timeout. Added comments and TODO for future.

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class PIDpyUI(QtWidgets.QMainWindow, dashboard.Ui_PIDpy):
         super(PIDpyUI, self).__init__(parent)
         self.setupUi(self)
 
+        # Setting default logging level
+        log.getLogger().setLevel(log.DEBUG)
+
         # Registering function calls for click actions
         self.kp.valueChanged.connect(self.kpchanged)
         self.ki.valueChanged.connect(self.kichanged)
@@ -20,7 +23,11 @@ class PIDpyUI(QtWidgets.QMainWindow, dashboard.Ui_PIDpy):
         # Initializing local variables
         self.Kp = self.Ki = self.Kd = self.SetPoint = 0
 
-        # Initialize
+        # Setup timer to update values on graph
+        self.timer = pg.QtCore.QTimer(self)
+
+        # Initialize terms for graphs
+        # Creating lists since plot function takes list as an input
         self.xAxis = list(range(100))
         self.pTerm = [0] * 100
         self.iTerm = [0] * 100
@@ -28,27 +35,86 @@ class PIDpyUI(QtWidgets.QMainWindow, dashboard.Ui_PIDpy):
         self.opTerm = [0] * 100
         self.errorTerm = [0] * 100
 
-        # See if this can be put in layout.py
-        outputPen = pg.mkPen(color=(255, 255, 255), width=2)
-        errorPen = pg.mkPen(color=(255, 255, 255), width=2)
-        kpPen = pg.mkPen(color=(255, 0, 0), width=2)
-        kiPen = pg.mkPen(color=(0, 255, 0), width=2)
-        kdPen = pg.mkPen(color=(0, 0, 255), width=2)
+        # Creating marker pens for each graph with different colors
+        self.outputpen = pg.mkPen(color=(255, 255, 255), width=2)
+        self.errorpen = pg.mkPen(color=(255, 255, 255), width=2)
+        self.kppen = pg.mkPen(color=(255, 0, 0), width=2)
+        self.kipen = pg.mkPen(color=(0, 255, 0), width=2)
+        self.kdpen = pg.mkPen(color=(0, 0, 255), width=2)
 
-        # Initial Plot
-        self.KpGraph.plot(self.xAxis, self.pTerm, pen=kpPen)
-        self.KiGraph.plot(self.xAxis, self.iTerm, pen=kiPen)
-        self.KdGraph.plot(self.xAxis, self.dTerm, pen=kdPen)
-        self.ErrorGraph.plot(self.xAxis, self.errorTerm, pen=errorPen)
-        self.OutputGraph.plot(self.xAxis, self.opTerm, pen=outputPen)
+        # Initial Plot on start-up
+        self.KpGraph.plot(self.xAxis, self.pTerm, pen=self.kppen)
+        self.KiGraph.plot(self.xAxis, self.iTerm, pen=self.kipen)
+        self.KdGraph.plot(self.xAxis, self.dTerm, pen=self.kdpen)
+        self.ErrorGraph.plot(self.xAxis, self.errorTerm, pen=self.errorpen)
+        self.OutputGraph.plot(self.xAxis, self.opTerm, pen=self.outputpen)
+
+    # Setting all controls for changing Ki, Kp, Kd and set point values
+    # to inactive to avoid any changes during the run
+    def setcontrolsinactive(self):
+        self.kp.setEnabled(False)
+        self.ki.setEnabled(False)
+        self.kd.setEnabled(False)
+        self.setpoint.setEnabled(False)
+
+    # Setting all controls back to active once the plotting is stopped
+    def setcontrolsactive(self):
+        self.kp.setEnabled(True)
+        self.ki.setEnabled(True)
+        self.kd.setEnabled(True)
+        self.setpoint.setEnabled(True)
+
+    # Initialize timer for certain milliseconds which calls registered function at timeout
+    def inittimer(self):
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.refreshplot)
+
+    # Start or stop the initiated timer
+    def toggletimer(self):
+        if not self.timer.isActive():
+            self.timer.start(0)
+        else:
+            self.timer.stop()
 
     def startclicked(self):
         if self.startButton.text() == "Start":
+            self.setcontrolsinactive()
+            self.inittimer()
+            self.toggletimer()
             self.startButton.setText("Stop")
             log.info("Start clicked")
         else:
+            self.setcontrolsactive()
+            self.toggletimer()
             self.startButton.setText("Start")
             log.info("Stop clicked")
+
+    # This function is called by timer everytime it times out.
+    # TODO: Read data from input pipe of given interface (COM port, UDP, TCP, WiFi, Bluetooth)
+    #       Compute Error and controller output
+    #       Update all value lists for plotting
+    #       Plot all values
+    def refreshplot(self):
+        # Update all value lists for plotting
+        self.pTerm = self.pTerm[1:]
+        self.pTerm.append(self.xAxis[-1])
+        self.iTerm = self.iTerm[1:]
+        self.iTerm.append(self.xAxis[-1] - self.xAxis[-2])
+        self.dTerm = self.dTerm[1:]
+        self.dTerm.append(self.xAxis[-2] + self.xAxis[-3])
+        self.xAxis = self.xAxis[1:]
+        self.xAxis.append(self.xAxis[-1] + 1)
+        self.errorTerm = self.errorTerm[1:]
+        self.errorTerm.append(self.pTerm[-1] - self.iTerm[-1] - self.dTerm[-1])
+        self.opTerm = self.opTerm[1:]
+        self.opTerm.append(self.errorTerm[-1])
+
+        # Plot all values
+        self.KpGraph.plot(self.xAxis, self.pTerm, pen=self.kppen)
+        self.KiGraph.plot(self.xAxis, self.iTerm, pen=self.kipen)
+        self.KdGraph.plot(self.xAxis, self.dTerm, pen=self.kdpen)
+        self.ErrorGraph.plot(self.xAxis, self.errorTerm, pen=self.errorpen)
+        self.OutputGraph.plot(self.xAxis, self.opTerm, pen=self.outputpen)
 
     def kpchanged(self):
         log.info(f"Kp value :{self.kp.value()}")
