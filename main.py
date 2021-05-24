a1b2318530ac9d7ef5e617fab3f3c0bcc48060ec
Shivam Chauhan <iamchauhan55@gmail.com>
2021-05-23 17:34:32 -0700

    Changed object type of plotting panel from QGraphicsView to Widget which was promoted to PlotWidget. Plotting a default graph on startup.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
+import logging as log
 from PyQt5 import QtWidgets
+import pyqtgraph as pg
 from GUI import dashboard
 
 
@@ -18,20 +20,47 @@ class PIDpyUI(QtWidgets.QMainWindow, dashboard.Ui_PIDpy):
         # Initializing local variables
         self.Kp = self.Ki = self.Kd = self.SetPoint = 0
 
+        # Initialize
+        self.xAxis = list(range(100))
+        self.pTerm = [0] * 100
+        self.iTerm = [0] * 100
+        self.dTerm = [0] * 100
+        self.currentTerm = [0] * 100
+        self.errorTerm = [0] * 100
+
+        # See if this can be put in layout.py
+        penUpLeft = pg.mkPen(color=(255, 255, 255), width=2)
+        penUpRight = pg.mkPen(color=(255, 255, 255), width=2)
+        penBottomLeft = pg.mkPen(color=(255, 0, 0), width=2)
+        penBottomCentre = pg.mkPen(color=(0, 255, 0), width=2)
+        penBottomRight = pg.mkPen(color=(0, 0, 255), width=2)
+
+        # Initial Plot
+        self.UpLeft = self.KpGraph.plot(self.xAxis, self.currentTerm, pen=penUpLeft)
+        self.UpRight = self.KiGraph.plot(self.xAxis, self.errorTerm, pen=penUpRight)
+        self.BottomLeft = self.KdGraph.plot(self.xAxis, self.pTerm, pen=penBottomLeft)
+        self.BottomCenter = self.ErrorGraph.plot(self.xAxis, self.iTerm, pen=penBottomCentre)
+        self.BottomRight = self.OutputGraph.plot(self.xAxis, self.dTerm, pen=penBottomRight)
+
     def startclicked(self):
-        print("start clicked")
+        if self.startButton.text() == "Start":
+            self.startButton.setText("Stop")
+            log.info("Start clicked")
+        else:
+            self.startButton.setText("Start")
+            log.info("Stop clicked")
 
     def kpchanged(self):
-        print(f"Kp value :{self.kp.value()}")
+        log.info(f"Kp value :{self.kp.value()}")
 
     def kichanged(self):
-        print(f"Ki value :{self.ki.value()}")
+        log.info(f"Ki value :{self.ki.value()}")
 
     def kdchanged(self):
-        print(f"Kd value :{self.kd.value()}")
+        log.info(f"Kd value :{self.kd.value()}")
 
     def setpointchanged(self):
-        print(f"Setpoint value :{self.setpoint.value()}")
+        log.info(f"Setpoint value :{self.setpoint.value()}")
 
 
 if __name__ == '__main__':
